a11e2bd31b00aa6437529f25fd57d455a42c0cfd
SarahRubio <sarahrubio.pro@gmail.com>
2020-03-19 21:11:49 +0100

    test js legend

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,11 @@
-console.log("alert");
+document.querySelector(".red").addEventListener("mouseenter", function () {
+  document.querySelector(".ligne").style.visibility="visible";
+  document.querySelector(".film").style.visibility="visible";
+  document.querySelector(".triangle").style.visibility="visible";
+});
 
-document.querySelector(".yellow").addEventListener("mouseenter", function () {
-  document.querySelector(".button").style.display="inline";
+document.querySelector(".red").addEventListener("mouseout", function () {
+  document.querySelector(".ligne").style.visibility="hidden";
+  document.querySelector(".film").style.visibility="hidden";
+  document.querySelector(".triangle").style.visibility="hidden";
 });
