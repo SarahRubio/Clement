e4587702daf79439a4c90a26a8a9b5ec4bd7d112
SarahRubio <sarahrubio.pro@gmail.com>
2020-05-27 13:57:57 +0200

    essai fix slideshow javascript

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -33,8 +33,7 @@ function animated_text1() {
 }
 animated_text1()
 
-
-// Affichage path (refactorisation)
+// Affichage path
 
 function RendreVisible(classPath) {
   document.querySelectorAll(`[data-color="${classPath}"]`).forEach(function(path){
@@ -43,7 +42,7 @@ function RendreVisible(classPath) {
 };
 
 function RendreInvisible(classPath) {
-  document.querySelectorAll('[data-color="' + classPath + '"]').forEach(function(path){
+  document.querySelectorAll(`[data-color="${classPath}"]`).forEach(function(path){
     path.style.visibility="hidden";
   });
 };
@@ -60,104 +59,146 @@ document.querySelector('.red').addEventListener("mouseout", function () {
 });
 }
 
-// SHOOT rotation NY
-//
-// function startTrombi() {
-//   setTimeout(trombiDynamique(), 2000);
-//   let timer = setTimeout(trombiDynamique(), 2000);
-// }
-//
-// function stopTrombi() {
-//   clearTimeOut(timer);
-// }
-//
-// function trombiDynamique() {
-//
-//   imageVisible = '<img border="0" src="'+pictures[chiffre]+'">';
-//
-//   document.getElementById("box").innerHTML = imageVisible;
-//
-//   if(chiffre == longueur) {
-//     chiffre = 0;
-//   } else {
-//     chiffre++;
-//   };
-//
-// }
-//
-// document.querySelector('#box').addEventListener('mouseenter', function () {
-//   stopTrombi();
-// })
-//
-// document.querySelector('#box').addEventListener('mouseout', function () {
-//   startTrombi();
-// })
-//
-//   pictures = new Array()
-//   pictures[0] = 'assets/images/newyork1.png';
-//   pictures[1] = 'assets/images/newyork2.png';
-//   pictures[2] = 'assets/images/newyork3.png';
-//   pictures[3] = 'assets/images/newyork4.png';
-//   pictures[4] = 'assets/images/newyork5.png';
-//   longueur = pictures.length-1;
-//
-//   liens = new Array()
-//   liens[0] = 'lien_image1.html';
-//   liens[1] = 'lien_image2.html';
-//   liens[2] = 'lien_image3.html';
-//   liens[3] = 'lien_image4.html';
-//   liens[4] = 'lien_image5.html';
-//   chiffre = 0;
+// SLIDESHOW rotation NEWYORK SANS POSSIBILITE ARRET SUR IMAGE.
 
-// let keepgoing = true;
-//
-// if(document.querySelector('.newyork')){
-// function trombiDynamique1() {
-//
-//   imageVisible = '<img border="0" src="'+ pictures[chiffre] +'">';
-//   document.getElementById("box").innerHTML = imageVisible;
-//   if(chiffre == longueur)
-//       { chiffre = 0; }
-//       else
-//       { chiffre++; }
-//   changement = 1;
-//
-//   if(keepgoing){
-//     window.setTimeout("trombiDynamique1()", (changement*2000));
-//   }
-//
-// }
-//
-// pictures = new Array()
-// pictures[0] = 'assets/images/newyork1.png';
-// pictures[1] = 'assets/images/newyork2.png';
-// pictures[2] = 'assets/images/newyork3.png';
-// pictures[3] = 'assets/images/newyork4.png';
-// pictures[4] = 'assets/images/newyork5.png';
-// longueur = pictures.length-1;
-//
-// liens = new Array()
-// liens[0] = 'lien_image1.html';
-// liens[1] = 'lien_image2.html';
-// liens[2] = 'lien_image3.html';
-// liens[3] = 'lien_image4.html';
-// liens[4] = 'lien_image5.html';
-// chiffre = 0;
-//
-// function startTrombi1 () {
-//   keepgoing = true;
-//   trombiDynamique1();
-// }
-//
-// function stopTrombi () {
-//   keepgoing = false;
-// }
-//
-// document.querySelector('#box').addEventListener('mouseenter', function () {
-//   stopTrombi();
-// })
-//
-// document.querySelector('#box').addEventListener('mouseout', function () {
-//   startTrombi1();
-// })
-// }
+function trombiDynamique() {
+
+      imageVisible = '<img border="0" src="'+pictures[chiffre]+'">';
+      document.getElementById('box').innerHTML = imageVisible;
+      if(chiffre == longueur) {
+        chiffre = 0;
+      } else {
+        chiffre++;
+      }
+      changement = 1;
+      window.setTimeout("trombiDynamique()", (changement*2000));
+}
+
+    pictures = new Array()
+    pictures[0] = 'assets/images/newyork1.png';
+    pictures[1] = 'assets/images/newyork2.png';
+    pictures[2] = 'assets/images/newyork3.png';
+    pictures[3] = 'assets/images/newyork4.png';
+    pictures[4] = 'assets/images/newyork5.png';
+    longueur = pictures.length-1;
+
+    liens = new Array()
+    liens[0] = 'lien_image1.html';
+    liens[1] = 'lien_image2.html';
+    liens[2] = 'lien_image3.html';
+    liens[3] = 'lien_image4.html';
+    liens[4] = 'lien_image5.html';
+    chiffre = 0;
+
+
+// DEUXIEME ESSAI SLIDESHOW AVEC POSSIBILITE ARRET SUR IMAGE
+
+        // function startTrombi() {
+        //   setTimeout(trombiDynamique(), 2000);
+        //   let timer = setTimeout(trombiDynamique(), 2000);
+        // }
+
+        // function stopTrombi() {
+        //   clearTimeOut(timer);
+        // }
+
+        // function trombiDynamique() {
+        //
+        //   imageVisible = '<img border="0" src="'+pictures[chiffre]+'">';
+        //
+        //   document.getElementById("box").innerHTML = imageVisible;
+        //
+        //   if(chiffre == longueur) {
+        //     chiffre = 0;
+        //   } else {
+        //     chiffre++;
+        //   };
+        //
+        // }
+
+        // document.querySelector('#box').addEventListener('mouseenter', function () {
+        //   stopTrombi();
+        // })
+        //
+        // document.querySelector('#box').addEventListener('mouseout', function () {
+        //   startTrombi();
+        // })
+
+        //   pictures = new Array()
+        //   pictures[0] = 'assets/images/newyork1.png';
+        //   pictures[1] = 'assets/images/newyork2.png';
+        //   pictures[2] = 'assets/images/newyork3.png';
+        //   pictures[3] = 'assets/images/newyork4.png';
+        //   pictures[4] = 'assets/images/newyork5.png';
+        //   longueur = pictures.length-1;
+        //
+        //   liens = new Array()
+        //   liens[0] = 'lien_image1.html';
+        //   liens[1] = 'lien_image2.html';
+        //   liens[2] = 'lien_image3.html';
+        //   liens[3] = 'lien_image4.html';
+        //   liens[4] = 'lien_image5.html';
+        //   chiffre = 0;
+        //
+        // let keepgoing = true;
+
+// TROISIEME ESSAI SLIDESHOW
+
+        // if(document.querySelector('.newyork')) {
+        //     function trombiDynamique() {
+        //
+        //       imageVisible = '<img border="0" src="'+ pictures[chiffre] +'">';
+        //       document.getElementById("box").innerHTML = imageVisible;
+        //       if(chiffre == longueur)
+        //           { chiffre = 0; }
+        //           else
+        //           { chiffre++; }
+        //       changement = 1;
+        //
+        //       if(keepgoing){
+        //         window.setTimeout("trombiDynamique1()", (changement*2000));
+        //       }
+        //
+        //     }
+        //
+        // pictures = new Array()
+        // pictures[0] = 'assets/images/newyork1.png';
+        // pictures[1] = 'assets/images/newyork2.png';
+        // pictures[2] = 'assets/images/newyork3.png';
+        // pictures[3] = 'assets/images/newyork4.png';
+        // pictures[4] = 'assets/images/newyork5.png';
+        // longueur = pictures.length-1;
+        //
+        // liens = new Array()
+        // liens[0] = 'lien_image1.html';
+        // liens[1] = 'lien_image2.html';
+        // liens[2] = 'lien_image3.html';
+        // liens[3] = 'lien_image4.html';
+        // liens[4] = 'lien_image5.html';
+        // chiffre = 0;
+        //
+        // }
+        //
+        //
+        //
+        // // J'arrête le trombi quand la souris survole l'image.
+        //
+        // function stopTrombi () {
+        //   keepgoing = false;
+        // }
+        //
+        // document.querySelector('#box').addEventListener('mouseenter', function () {
+        //   stopTrombi();
+        // })
+        //
+        // // Je reprends le trombi quand la souris sort de l'image
+        //
+        // function startTrombi () {
+        //   keepgoing = true;
+        //   trombiDynamique1();
+        // }
+        //
+        // document.querySelector('#box').addEventListener('mouseout', function () {
+        //   startTrombi();
+        // })
+        // }
